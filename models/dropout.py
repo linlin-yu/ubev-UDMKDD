70c1a27d893a54ef43fe9a4d54299b6e7a7d75d6
bluffish <bowenymail@gmail.com>
2023-07-28 13:28:20 -0500

    update code (drpt, pstnt, ensmbl, gcd)

---
+++ models/dropout.py
@@ -0,0 +1,35 @@
+from focal_loss.focal_loss import FocalLoss
+
+from models.model import Model
+from tools.loss import *
+from tools.uncertainty import *
+
+
+class Baseline(Model):
+    def __init__(self, *args, **kwargs):
+        super(Baseline, self).__init__(*args, **kwargs)
+
+    @staticmethod
+    def aleatoric(logits):
+        return entropy(logits)
+
+    @staticmethod
+    def epistemic(logits):
+        return entropy(logits)
+
+    @staticmethod
+    def activate(logits):
+        return torch.softmax(logits, dim=1)
+
+    def loss(self, logits, target):
+        if self.loss_type == 'ce':
+            return ce_loss(logits, target, weights=self.weights).mean()
+        elif self.loss_type == 'focal':
+            return focal_loss(logits, target, weights=self.weights, n=2).mean()
+        elif self.loss_type == 'al':
+            return a_loss(logits, target, weights=self.weights).mean()
+        else:
+            raise NotImplementedError()
+
+    def forward(self, images, intrinsics, extrinsics):
+        return self.backbone(images, intrinsics, extrinsics)
---
+++ models/postnet.py
@@ -0,0 +1,74 @@
+from models.model import Model
+from tools.loss import *
+from tools.uncertainty import *
+
+
+class Evidential(Model):
+    def __init__(self, *args, **kwargs):
+        super(Evidential, self).__init__(*args, **kwargs)
+
+    @staticmethod
+    def aleatoric(alpha):
+        return dissonance(alpha)
+
+    @staticmethod
+    def epistemic(alpha):
+        return vacuity(alpha)
+
+    @staticmethod
+    def activate(alpha):
+        return alpha / torch.sum(alpha, dim=1, keepdim=True)
+
+    def loss(self, alpha, y, beta_lambda=.0005):
+        if self.loss_type == 'ce':
+            A = uce_loss(alpha, y, weights=self.weights)
+        elif self.loss_type == 'focal':
+            A = u_focal_loss(alpha, y, weights=self.weights, n=2)
+        else:
+            raise NotImplementedError()
+
+        if beta_lambda > 0:
+            A += entropy_reg(alpha, beta_lambda)
+
+        return A.mean()
+
+    def loss_ood(
+        self, alpha, y, ood,
+        beta_lambda=.0005,
+        ood_lambda=.0005
+    ):
+        A = uce_loss(alpha, y, self.weights)
+
+        if beta_lambda > 0:
+            A += entropy_reg(alpha, beta_reg=beta_lambda)
+
+        A = A[(1 - ood).unsqueeze(1).bool()].mean()
+        
+        if ood_lambda > 0:
+            A += ood_reg(alpha, ood) * ood_lambda
+
+        return A
+
+    def train_step_ood(self, images, intrinsics, extrinsics, labels, ood):
+        self.opt.zero_grad(set_to_none=True)
+
+        outs = self(images, intrinsics, extrinsics)
+        preds = self.activate(outs)
+
+        loss = self.loss_ood(outs, labels.to(self.device), ood)
+        loss.backward()
+
+        nn.utils.clip_grad_norm_(self.parameters(), 5.0)
+        self.opt.step()
+
+        return outs, preds, loss
+
+    def forward(self, images, intrinsics, extrinsics, limit=None):
+        evidence = self.backbone(images, intrinsics, extrinsics).relu()
+
+        if limit is not None:
+            evidence = evidence.clamp(max=limit)
+        alpha = evidence + 1
+
+        return alpha
+
